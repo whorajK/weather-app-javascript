0ee5dd279bbcc51de0a290d5792c13739c13fd02
Raj Changappa <102789104+whorajK@users.noreply.github.com>
2023-01-07 14:23:35 +0530

    added gitignore file

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+  API_KEY: "769ad78edf98585e77c0d5953fe7f395",
+};
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const APIKEY = "769ad78edf98585e77c0d5953fe7f395";
-
 // VARIABLES
+const apiKey = config.API_KEY;
 const form = document.querySelector("#form");
 const searchInputEl = document.querySelector("#search");
 
 const tempEl = document.querySelector("#temp");
 const locationEl = document.querySelector("#location");
 const iconEl = document.querySelector("#icon");
-const timeEl = document.querySelector("#time");
-const dateEl = document.querySelector("#date");
 
 const conditionEl = document.querySelector("#condition");
 const humidityEl = document.querySelector("#humidity");
 const windSpeedEl = document.querySelector("#wind-speed");
+
+const weatherBox = document.querySelector(".weather-box--container");
+const msg = document.querySelector(".msg");
 
 // SUBMITS USER REQUEST
 form.addEventListener("submit", (e) => {
@@ -23,13 +23,18 @@ form.addEventListener("submit", (e) => {
     alert("enter city");
   } else {
     getWeatherData(city);
+    weatherBox.classList.remove("hidden");
+    msg.classList.add("hidden");
+
+    const container = document.querySelector(".icon");
+    container.classList.add("bg");
   }
 });
 
 // GETS WEATHER DATA FROM THE API
 let getWeatherData = async (city) => {
   await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -44,21 +49,59 @@ let getWeatherData = async (city) => {
       locationEl.innerText = nameText;
       conditionEl.innerText = description;
       humidityEl.innerText = `${humidity}%`;
-      windSpeedEl.innerText = `${speed} kmph`;
+      windSpeedEl.innerText = `${speed} mps`;
     });
 };
 
-// let dayandTime
-// const weekday = [
-//   "Sunday",
-//   "Monday",
-//   "Tuesday",
-//   "Wednesday",
-//   "Thursday",
-//   "Friday",
-//   "Saturday",
-// ];
+let getDate = () => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-// let date = new Date();
-// // dateEl.innerText = date.getDay()
-// console.log(weekday[date.getDay()]);
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const dateEl = document.querySelector("#date");
+  const timeEl = document.querySelector("#time");
+
+  let date = new Date();
+
+  dateEl.innerText = `${weekday[date.getDay()]} ${date.getDate()} ${
+    months[date.getMonth()]
+  } ${date.getFullYear()}`;
+
+  let hourEl = document.createElement("span");
+  hourEl.innerText = `${date.getHours()}`;
+
+  let minuteEl = document.createElement("span");
+  minuteEl.innerText = `${date.getMinutes()}`;
+
+  timeEl.appendChild(hourEl, minuteEl);
+  timeEl.innerHTML = `${hourEl.innerText}: ${minuteEl.innerText}`;
+
+  if (hourEl.innerText < "18") {
+    document.body.style.backgroundImage = `url(./svgs/Cloudy.svg)`;
+  } else {
+    document.body.style.backgroundImage = `url(./svgs/Cloudydark.svg)`;
+  }
+};
+
+getDate();
